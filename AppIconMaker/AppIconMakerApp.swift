eaa4315707c406c78b7eba3cc6b08de9df892df7
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2025-10-21 14:17:19 +0900

    initial commit

---
+++ AppIconMaker/AppIconMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIconMakerApp.swift
+//  AppIconMaker
+//
+//  Created by uhimania on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AppIconMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
